fa3c76000668a8e55a91760541fec4025cb676fb
Anmol Agrawal <anmol.ag53@gmail.com>
2022-08-27 14:51:55 +0530

    Fixed: convertToUTC function

--- src/index.js
+++ src/index.js
@@ -77,52 +77,13 @@ const convertFromUTC = ({ date, utc_offset, zone } = {}) => {
   };
 };
 
-const convertToUTC = ({ date, utc_offset, zone } = {}) => {
+const convertToUTC = ({ date } = {}) => {
   if (!(date instanceof Date)) {
     return { status: "Invalid Date" };
   }
-  const offsetRegex = /^([+-](?:2[0-3]|[01][0-9]):[0-5][0-9])$/;
-  if (utc_offset && !offsetRegex.test(utc_offset)) {
-    return { status: "Invalid Offset" };
-  }
-  if (utc_offset === "-00:00") {
-    utc_offset = "+00:00";
-  }
-  if (utc_offset && zone) {
-    const offset = getOffset(zone);
-    if (offset.status !== "OK") {
-      return { status: offset.status };
-    }
-    if (utc_offset !== offset.utc_offset) {
-      return { status: "utc_offset does not matched with zone" };
-    }
-  }
-  if (!utc_offset) {
-    if (!zone) {
-      return { status: "OK", iso_date: date.toISOString() };
-    }
-    const offset = getOffset(zone);
-    if (offset.status !== "OK") {
-      return { status: offset.status };
-    }
-    utc_offset = offset.utc_offset;
-  }
-  if (utc_offset === "+00:00" || utc_offset === "-00:00") {
-    return { status: "OK", iso_date: date.toISOString() };
-  }
-  let hourOffset = Number(utc_offset.substring(1, 3));
-  let minuteOffset = Number(utc_offset.substring(4, 6));
-  if (utc_offset[0] === "+") {
-    hourOffset *= -1;
-    minuteOffset *= -1;
-  }
-  const input_iso_date = date.toISOString();
-  const currentUTC = new Date(input_iso_date);
-  currentUTC.setHours(currentUTC.getHours() + hourOffset);
-  currentUTC.setMinutes(currentUTC.getMinutes() + minuteOffset);
   return {
     status: "OK",
-    iso_date: currentUTC.toISOString().replace("Z", utc_offset),
+    iso_date: date.toISOString(),
   };
 };
 
